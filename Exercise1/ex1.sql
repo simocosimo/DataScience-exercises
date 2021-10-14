00bf1e72a6ff3f5b9562bdf47a73ef130ff95828
Simone Cosimo <simosx4@gmail.com>
2021-10-14 10:54:50 +0200

    Missing semicolons lol

--- Exercise1/ex1.sql
+++ Exercise1/ex1.sql
@@ -17,7 +17,7 @@ FROM Sales S, Category C
 WHERE S.ItemCategoryID = C.CategoryID -- joins = n-1 (where n number of tables)
 -- filter conditions should go here (none in this example)
 GROUP BY CategoryName, CategoryID
-ORDER BY Rank_SoldItems
+ORDER BY Rank_SoldItems;
 -- I can only use, in the select, attributes that are not present in the group by. I can 
 -- only do that by using aggregation functions (SUM, AVG, RANK, etc...)
 
@@ -31,5 +31,5 @@ SELECT Province, Region,
 FROM Sales S, Customer C
 WHERE S.CustomerID = C.CustomerID
 -- filter conditions should go here (none in this example)
-GROUP BY Province, Region   
+GROUP BY Province, Region;
 -- I need the Region clause, otherwise I can't put it in the select
