067c9a4dd9ff711e676a0641f19c0d32bc5a9548
Simone Cosimo <simosx4@gmail.com>
2021-10-14 10:45:56 +0200

    Query #1 - Categories

---
+++ Exercise1/ex1.sql
@@ -0,0 +1,22 @@
+-- Convention: using year in every field
+-- WE USE THIS RIGHT HERE
+-- TIME(1, 10/2021, 4/2021, 2/2021, 2021)
+
+/* 
+-- this is another example of memorizing time, coming out from the ETL process
+TIME(TimeID, Month, M_O_Y)
+(1, 10/2021, 'October') */
+
+-- Query #1
+SELECT CategoryName, 
+    SUM(TotAmount),
+    SUM(NumSoldItems),
+    RANK() OVER (ORDER BY SUM(TotAmount) DESC) Rank_TotAmount,
+    RANK() OVER (ORDER BY SUM(NumSoldItems) DESC) Rank_SoldItems,
+FROM Sales S, Category C
+WHERE S.ItemCategoryID = C.CategoryID -- joins = n-1 (where n number of tables)
+-- filter conditions should go here (none in this example)
+GROUP BY CategoryName, CategoryID
+ORDER BY Rank_SoldItems
+-- I can only use, in the select, attributes that are not present in the group by. I can 
+-- only do that by using aggregation functions (SUM, AVG, RANK, etc...)
