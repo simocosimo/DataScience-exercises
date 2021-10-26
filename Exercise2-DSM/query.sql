c6a57d51de50eceaeb09d21ad7274c31d781952e
Simone Cosimo <simosx4@gmail.com>
2021-10-26 15:54:30 +0200

    Solved query 2 in two different methods

--- Exercise2-DSM/query.sql
+++ Exercise2-DSM/query.sql
@@ -9,3 +9,25 @@ FROM EXPORT E, DESTINATION D, TIMEDIM T, WINETYPE W
 WHERE E.DID = D.DID AND E.TID = T.TID AND E.WTID = W.WTID
     AND D.Continent = "Asia" AND W.DOC = True
 GROUP BY T.year, E.Package;
+
+--Query #2 (1)
+SELECT T.year, DISTINCT W.Region,
+    SUM(SUM(E.tot_income)) OVER (PARTITION BY W.Region, T.year)/
+    SUM(SUM(E.tot_liters)) OVER (PARTITION BY W.Region, T.year),
+    AVG(SUM(E.tot_liters)) OVER (PARTITION BY W.Region, T.year),
+    --didn't quite get the one below, TODO: asks prof
+    100 * SUM(SUM(E.tot_liters)) OVER (PARTITION BY W.Region, T.year)/
+    SUM(SUM(E.tot_liters)) OVER (PARTITION BY W.Geo_area, T.year)
+FROM EXPORT E, TIMEDIM T, WINERY W
+WHERE E.WID = W.WID AND E.TID = T.TID
+GROUP BY T.year, W.Region, W.Geo_area, W.Province;
+
+--Query #2 (2)
+SELECT T.year, W.Region,
+    SUM(E.tot_income) / SUM(E.tot_liters),
+    SUM(E.tot_liters) / COUNT(DISTINCT W.Province),
+    100 * SUM(E.tot_liters) /
+    SUM(SUM(E.tot_liters)) OVER (PARTITION BY W.Geo_area, T.year)
+FROM EXPORT E, TIMEDIM T, WINERY W
+WHERE E.WID = W.WID AND E.TID = T.TID
+GROUP BY T.year, W.Region, W.Geo_area;
